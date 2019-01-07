4d6ede326c65a8e920550736646414e5beeff272
LAPTOP-CVF082V0\UpstartCommerce <mayank.verma@upstartcommerce.com>
2019-01-07 16:46:33 +0530

    adding the current prod ngrok url

--- src/app/services/urls.ts
+++ src/app/services/urls.ts
@@ -2,8 +2,8 @@ export class UrlsSettings {
 
     public static  API_ENDPOINT='http://127.0.0.1:6666/api/';
     public static baseurl: string ='http://localhost:9000/upload/';
-    //public static prod_url: string ='https://c7d7e428.ngrok.io';
-    public static prod_url: string ='http://localhost:9000';
+    public static prod_url: string ='https://54c1cbbb.ngrok.io';
+    //public static prod_url: string ='http://localhost:9000';
     public static upload_url: string =UrlsSettings.prod_url+'/upload/';
 
 }
