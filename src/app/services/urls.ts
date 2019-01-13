63075c2613ae314dc0cb93639bca31cd58d7c514
LAPTOP-CVF082V0\UpstartCommerce <mayank.verma@upstartcommerce.com>
2019-01-13 21:28:04 +0530

    adding a new url

--- src/app/services/urls.ts
+++ src/app/services/urls.ts
@@ -2,7 +2,7 @@ export class UrlsSettings {
 
     public static  API_ENDPOINT='http://127.0.0.1:6666/api/';
     public static baseurl: string ='http://localhost:9000/upload/';
-    public static prod_url: string ='https://5d67566b.ngrok.io';
+    public static prod_url: string ='https://d561001d.ngrok.io';
     //public static prod_url: string ='http://localhost:9000';
     public static upload_url: string =UrlsSettings.prod_url+'/upload/';
 
